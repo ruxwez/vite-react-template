d55bbbb622020f6e64fcaa3ac43a93cfd1343963
Jose Garcia <47431411+ruxwez@users.noreply.github.com>
2023-08-05 15:58:23 +0200

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Router } from "@/routes";
+import { ThemeProvider } from "./contexts/theme.context";
 
 import "normalize.css";
 import "fix.css";
 import "@/styles/global.scss";
-import { ThemeProvider } from "./contexts/theme.context";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
