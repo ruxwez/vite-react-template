55f991aa68738a7eb8de44a8aa52a310004bec9c
Jose Garcia <47431411+ruxwez@users.noreply.github.com>
2023-08-05 15:31:46 +0200

    feat(route): 404

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,7 @@ export function Router() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<h1>Home</h1>} />
+                <Route path="*" element={<h1>404</h1>} />
             </Routes>
         </BrowserRouter>
     )
