f2a202a409c1205ddae0694756ee30cd088f6e93
Jose Garcia <47431411+ruxwez@users.noreply.github.com>
2023-08-05 21:06:05 +0200

    feat(constants): Create local storage Key Theme

--- src/constants/LocalStorageKeys.ts
+++ src/constants/LocalStorageKeys.ts
@@ -1,3 +1,5 @@
+// Enum para las claves de LocalStorage
 export enum LocalStorageKeys {
     AuthToken = "authToken",
+    Theme = "theme",
 }
--- src/contexts/theme.context.tsx
+++ src/contexts/theme.context.tsx
@@ -1,3 +1,4 @@
+import { LocalStorageKeys } from '@/constants/LocalStorageKeys'
 import { createContext, useEffect, useState } from 'react'
 
 interface ThemeContextProps {
@@ -9,20 +10,24 @@ interface ThemeContextState {
     setTheme: (theme: string) => void
 }
 
+// Creamos el ThemeContext 
 export const ThemeContext = createContext<ThemeContextState | undefined>(undefined)
 
+// Creamos el ThemeProvider
 export const ThemeProvider = (props: ThemeContextProps) => {
+    // Obtenemos el tema del localStorage o ponemos el por defecto
     const [theme, setT] = useState(window.localStorage.getItem('theme') || 'light')
-
+    
+    // Cambiamos el tema del body dependiendo del tema
     useEffect(() => {
         document.body.className = theme
     }, [theme])
 
+    // Funcion para cambiar el tema
     const setTheme = (theme: string) => {
         setT(theme)
-        localStorage.setItem("theme", theme)
+        localStorage.setItem(LocalStorageKeys.Theme, theme)
     }
-   
 
     return <ThemeContext.Provider value={{
         theme,
